0d2e59f17f8aefb51f2eea08db7ae048dd8a7dc4
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-09 16:00:24 +0900

    ex-review1: add PetroleumPrice class

---
+++ ex-review1/PetroleumPrice.java
@@ -0,0 +1,17 @@
+public class PetroleumPrice {
+  private Double gasolinePrice;
+  private Double dieselPrice;
+
+  public PetroleumPrice(Double gasolinePrice, Double dieselPrice) {
+    this.gasolinePrice = gasolinePrice;
+    this.dieselPrice = dieselPrice;
+  }
+
+  public Double getGasolinePrice() {
+    return this.gasolinePrice;
+  }
+
+  public Double getDieselCost() {
+    return this.dieselPrice;
+  }
+}
