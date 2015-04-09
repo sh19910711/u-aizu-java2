4f0ae6289b4aef7f3685d29e4452141acc250b1a
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-09 16:00:15 +0900

    ex-review1: add Truck class

---
+++ ex-review1/Truck.java
@@ -0,0 +1,25 @@
+public class Truck extends Vehicle {
+
+  private Integer numberOfSeats;
+  private Boolean airConditionOn;
+  private Integer power;
+
+  public Truck(String modelName, String company, String owner, String engineType, Double tankSize, Double fuelConsumption, Integer numberOfSeats, Integer power) {
+    super(modelName, company, owner, engineType, tankSize, fuelConsumption);
+    this.numberOfSeats = numberOfSeats;
+    this.power = power;
+  }
+
+  public Integer costFor100Km(PetroleumPrice p) {
+    return -1;
+  }
+
+  public void setAirConON() {
+    airConditionOn = true;
+  }
+
+  public void setAirConOFF() {
+    airConditionOn = false;
+  }
+
+}
