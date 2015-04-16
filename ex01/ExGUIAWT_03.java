e1c2ed0dcb80ffe903ccf586f8c519f7e9e740e0
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-16 16:08:34 +0900

    ex01-3: add ExGUIAWT_03 (not finished)

---
+++ ex01/ExGUIAWT_03.java
@@ -0,0 +1,48 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+<applet code="ExGUIAWT_03" archive="build/ex01.jar"
+  width="400" height="400"></applet>
+ */
+
+// This is an extension of ExGUIAWT_02. 
+// By horizontally dragging the mouse on the window, you can chagne the number quickly.
+// super class: ExGUIAWT_02
+// implement interface: MouseListener, MouseMotionListener
+public class ExGUIAWT_03 extends ExGUIAWT_02 implements MouseListener {
+  // Use an attribute to save mouse x position.
+  private Integer mouseX;
+
+  // Override Applet's init() method:
+  public void init() {
+    // (1) Call the same method of its supper class;
+    super.init();
+
+    // TODO: (2) Add listeners (this) to the ExGUIAWT_03 object itself
+    //     (use addMouseListener() and addMouseMotionListener()).
+    //     That is, we use two listeners to catch events on one object.
+  }
+
+  // Implement methods mousePressed() for MouseListener:
+  public void mousePressed(MouseEvent e) {
+    // TODO: (1) Get mouse x position (call getX() of the argument), save it to the attribute.
+  }
+
+  // Implement method mouseDragged() for MouseMotionListener:
+  public void mouseDragged(MouseEvent e) {
+    // TODO: (1) Get mouse x position; Compare it with the attribute (the saved position) to decide how to change the number in super class.
+    // TODO: (2) Set the number to the label in the super class.
+    // TODO: (3) Save current mouse position to the attribute for further use.
+  }
+
+  // Implement the following methods but do nothing:
+  // mouseClicked(),  mouseEntered(), mouseExited(), mouseReleased(), and mouseMoved().
+  public void mouseClicked  (MouseEvent e) {}
+  public void mouseEntered  (MouseEvent e) {}
+  public void mouseExited   (MouseEvent e) {}
+  public void mouseReleased (MouseEvent e) {}
+  public void mouseMoved    (MouseEvent e) {}
+}
+
