945dbb6a2b0ffd755eda1898f573d75aabbdd6f0
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-23 15:40:31 +0900

    ex02: show buttons on ExGUISwing_02

--- ex02/ExGUISwing_02.java
+++ ex02/ExGUISwing_02.java
@@ -1,6 +1,10 @@
 //
 // This is an extension of ExGUISwing_01, a window showing a central display and 6 buttons. The display has a border.
 //
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
 //
 // - class name: ExGUISwing_02 (file name: ExGUISwing_02.java)
@@ -8,12 +12,18 @@
 //
 public class ExGUISwing_02 extends ExGUISwing_01 {
 
-  // TODO: - Use a JButton object for each button
-  // TODO: - Use a JPanel object and a BevelBorder object for the central display
-  // TODO: - For showing text on the central display, use JLabel object
-  // TODO: - Create above JButton, JLabel and JPanel objects as attributes for future use.
+  // - Use a JButton object for each button
+  // - Use a JPanel object and a BevelBorder object for the central display
+  // - For showing text on the central display, use JLabel object
+  // - Create above JButton, JLabel and JPanel objects as attributes for future use.
+  protected JButton[]     buttons;
+  protected JPanel        panel;
+  protected BevelBorder   border;
+  protected JLabel        label;
 
-  // TODO: - In the constructor:
+
+  //
+  // - In the constructor:
   //   (1) Call the constructor of its super class.
   //   (2) Get the Container object of the JFrame  (by getContentPane() of JFrame)
   //   (3) Remove the layout manager of the Container and JPanel objects by calling their setLayout(null).
@@ -28,11 +38,59 @@ public class ExGUISwing_02 extends ExGUISwing_01 {
   //   (7) Add the JButton and JPanel objects to the Container object (use add() method).
   //   (8) Change the JLabel object's foreground color to dark green.
   //   (9) Add the JLabel object  to the JPanel object.
+  //
+  public ExGUISwing_02() {
+    // (1) Call the constructor of its super class.
+    super();
+
+    // (2) Get the Container object of the JFrame  (by getContentPane() of JFrame)
+    Container container = getContentPane();
+
+    // (3) Remove the layout manager of the Container and JPanel objects by calling their setLayout(null).
+    //        (This is to control the location and the size of the buttons and text by ourselves)
+    setLayout(null);
+
+    // TODO: (4) For each created object:
+    //      Set the size (use setSize() method). 
+    //      Set the location (use setLocation() method).
+    buttons = new JButton[4];
+    buttons[0] = new JButton("a");
+    buttons[1] = new JButton("b");
+    buttons[2] = new JButton("c");
+    buttons[3] = new JButton("d");
+    for ( int i = 0; i < 4; ++ i ) {
+      buttons[i].setSize(80, 40);
+      buttons[i].setLocation(100 * i + 10, 10);
+      container.add(buttons[i]);
+    }
+
+    // TODO: (5) For the JPanel object:
+    //       Create a BevelBorder object and set it to the JPanel by its setBorder() method.
+    border = new BevelBorder(BevelBorder.RAISED);
+    panel = new JPanel();
+    panel.setBorder(border);
 
 
-  // TODO: - Implement the main() method:
+    // TODO: (6) For the JLabel object:
+    //       Create a Font object and set it to the JLabel (use setFont()).
+    
+    // TODO: (7) Add the JButton and JPanel objects to the Container object (use add() method).
+    
+    // TODO: (8) Change the JLabel object's foreground color to dark green.
+    
+    // TODO: (9) Add the JLabel object  to the JPanel object.
+  }
+
+  // - Implement the main() method:
   //   (1) Create a ExGUISwing_02 object.
   //   (2) Call ExGUISwing_02's setVisible() with parameter "true".
+  public static void main(String argv[]) {
+    // (1) Create a ExGUISwing_02 object.
+    JFrame f = new ExGUISwing_02();
+
+    // (2) Call ExGUISwing_02's setVisible() with parameter "true".
+    f.setVisible(true);
+  }
 
 }
 
