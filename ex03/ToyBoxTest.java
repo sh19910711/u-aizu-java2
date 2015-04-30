f934e96e24e3f53cd057a1c3d4fcb2a42fca03b2
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-30 15:54:43 +0900

    ex03: edit ToyBoxTest

--- ex03/ToyBoxTest.java
+++ ex03/ToyBoxTest.java
@@ -4,70 +4,72 @@ import java.util.ArrayList;
  * ToyBox Example.
  */
 
+// (2) There is an incomplete code "ToyBoxTest.java" on your work directory.
+// It has a generic type "ToyBox", and shows some ways how to use this type. Complete the code to show the same result as ToyBoxTestAnswer does. 
 
 class ToyBox<T> {
 
-    private ArrayList<T> v ..... // Complete here to assign ArrayList object         
+  private ArrayList<T> v; // ..... // Complete here to assign ArrayList object         
 
     public void add() {
-        .......    // Complete add method with proper argument and method body                     
+      // .......    // Complete add method with proper argument and method body                     
     }
 
-    public T get() {
-        .......    // Complete get method with proper argument and method body                     
-    }
+  public T get() {
+    // .......    // Complete get method with proper argument and method body                     
+  }
 
-    public int getSize() {
-        .......    // Complete getSize method with proper argument and method body                     
-    }
+  public int getSize() {
+    // .......    // Complete getSize method with proper argument and method body                     
+  }
 }
 
 public class ToyBoxTest {
-    public static void showToysinBox(/* Parameter with bounded wildcard to accept Toy and its subclasses*/) {
-        .......    // Complete showAnimalsinToyBox method with proper argument and method body                     
-    }
+  public static void showToysinBox(/* Parameter with bounded wildcard to accept Toy and its subclasses*/) {
+    // .......    // Complete showAnimalsinToyBox method with proper argument and method body                     
+  }
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-     // Create a "carBox" with proper sentence
-       ........................................
+    // Create a "carBox" with proper sentence
+    // ........................................
 
-       Car t1 = new Car("Yaris", 1500);
-       Car t2 = new Car("Corolla", 2500);
-      carBox.add(t1);
-      carBox.add(t2);
-      showToysinBox(carBox);
+    Car t1 = new Car("Yaris", 1500);
+    Car t2 = new Car("Corolla", 2500);
+    carBox.add(t1);
+    carBox.add(t2);
+    showToysinBox(carBox);
 
-     // Create a "bearBox" with proper sentence
-       ........................................
-       Bear b1 = new Bear("Bear1", 1000);
-       Bear b2 = new Bear("Bear2", 2000);
-      bearBox.add(b1);
-      bearBox.add(b2);
-      showToysinBox(bearBox);
+    // Create a "bearBox" with proper sentence
+    // ........................................
+    Bear b1 = new Bear("Bear1", 1000);
+    Bear b2 = new Bear("Bear2", 2000);
+    bearBox.add(b1);
+    bearBox.add(b2);
+    showToysinBox(bearBox);
 
 
-      /* It is OK till now */
+    /* It is OK till now */
 
-      // Next, create Box<Toy>
+    // Next, create Box<Toy>
 
-     // Create a "toyBox" with proper sentence
-       ........................................
+    // Create a "toyBox" with proper sentence
+    // ........................................
 
-       Car t3 = new Car("Tacoma", 3300);
-       Bear b3 = new Bear("Bear3", 1200);
+    Car t3 = new Car("Tacoma", 3300);
+    Bear b3 = new Bear("Bear3", 1200);
 
-      toyBox.add(t3);
-      toyBox.add(b3);
+    toyBox.add(t3);
+    toyBox.add(b3);
 
-      showToysinBox(toyBox);
-      /* It is still OK till now */
+    showToysinBox(toyBox);
+    /* It is still OK till now */
 
-      // How about the next? Is it subtype?
-      ToyBox</* Fill in here with proper wildcard */> sometoyBox = carBox;
-      System.out.println("After assigning the carBox into the \"sometoyBox\"...");
-      showToysinBox(sometoyBox);
-    }
+    // How about the next? Is it subtype?
+    ToyBox</* Fill in here with proper wildcard */> sometoyBox = carBox;
+    System.out.println("After assigning the carBox into the \"sometoyBox\"...");
+    showToysinBox(sometoyBox);
+  }
 }
 
 class Toy {
