37589fce071e73132d8b332fe3072c65d14e99fe
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-30 14:59:57 +0900

    ex03: copy ExGenerics_01.java

---
+++ ex03/ExGenerics_01.java
@@ -0,0 +1,31 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+
+/*
+<applet code = "ExGenerics_01" width=400 height=400></applet>
+*/
+
+public class ExGenerics_01 extends Applet{
+	ArrayList num_list = new ArrayList();
+
+	public void init(){
+		this.setSize(400, 300);
+		num_list.add("1200");
+		num_list.add(new Point(300, 100));
+		num_list.add(1300);
+		num_list.add("1400");
+		num_list.add("1500");
+		num_list.add("1600");
+	}
+
+	public void paint(Graphics g){
+		super.paint(g);
+
+		for(int ii = 0 ; ii < num_list.size() ; ii++){
+			String s = (String)num_list.get(ii);
+			g.drawString(s, 100, ii*20);
+		}
+	}
+}
