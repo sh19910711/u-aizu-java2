da4ceae98dce9eee4add07391083c6f9dfe30ac4
Hiroyuki Sano <sh19910711@gmail.com>
2015-06-04 15:32:33 +0900

    ex06: add ThreadPiMC class

--- ex06/ThreadPiMC.java
+++ ex06/ThreadPiMC.java
@@ -1,16 +1,26 @@
 import java.util.Random;
 
-public class PiMC {
-  // Estimate Pi by computing the area of a unit disk
-  public static double estimateMC(int num_samples) {
-    // disk radius
-    double r = 1.0;
-    double sum = 0.0;
+public class ThreadPiMC implements Runnable {
 
+  private static final int NUM_THREAD = 8;
+
+  // disk radius
+  private static final double r = 1.0;
+  private static double totalSum = 0.0;
+  private static int totalNumSamples = 0;
+
+  private int numSamples = 0;
+
+  public ThreadPiMC(int numSamples) {
+    this.numSamples = numSamples;
+  }
+
+  public double doMonteCarlo(int numSamples) {
+    double sum = 0.0;
     // Pseudo random number generator
     Random rng = new Random();
 
-    for (int i = 0; i < num_samples; ++i) {
+    for (int i = 0; i < numSamples; ++i) {
       // Generate random samples from uniform distribution [-1,1]
       double xs = -1.0 + 2.0*rng.nextDouble();
       double ys = -1.0 + 2.0*rng.nextDouble();
@@ -20,7 +30,13 @@ public class PiMC {
       }
     }
 
-    return (4.0 / num_samples * sum);
+    // return (4.0 / num_samples * sum);
+    return sum;
+  }
+
+  public void run() {
+    double sum = doMonteCarlo(numSamples);
+    totalSum += sum;
   }
 
   public static void main(String[] args) {
@@ -29,8 +45,41 @@ public class PiMC {
       System.exit(1);
     }
 
-    int num_samples = Integer.parseInt(args[0]);
-    double pi_tilde = estimateMC(num_samples);
-    System.out.println("Pi ~ " + pi_tilde);
+    totalNumSamples = Integer.parseInt(args[0]);
+    int numThreadSamples = totalNumSamples / NUM_THREAD;
+
+    // Prepare threads
+    Runnable[] runnables = new Runnable[NUM_THREAD];
+    for ( int i = 0; i < NUM_THREAD; ++ i ) {
+      if ( i + 1 < NUM_THREAD ) {
+        runnables[i] = new ThreadPiMC(numThreadSamples);
+      } else {
+        runnables[i] = new ThreadPiMC(numThreadSamples + totalNumSamples % numThreadSamples);
+      }
+    }
+
+    // Create threads
+    Thread[] threads = new Thread[NUM_THREAD];
+    for ( int i = 0; i < NUM_THREAD; ++ i ) {
+      threads[i] = new Thread(runnables[i]);
+    }
+
+    // Run threads
+    for ( int i = 0; i < NUM_THREAD; ++ i ) {
+      threads[i].start();
+    }
+
+    // Wait threads
+    try {
+      for ( int i = 0; i < NUM_THREAD; ++ i ) {
+        threads[i].join();
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+
+    // Merge result: Calculate PI
+    double piTilde = 4.0 / totalNumSamples * totalSum;
+    System.out.println("Pi ~ " + piTilde);
   }
 }
